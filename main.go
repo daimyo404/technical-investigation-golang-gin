28165b0442facee9be26d0de6900505088c6ce7d
saito-hiroaki <hiro021312@gmail.com>
2022-11-03 23:57:40 +0900

    initial commit.

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type fugaType struct {
+	id      string
+	message string
+}
+
+var fugavar = fugaType{
+	id:      "fuga",
+	message: "fugafuga",
+}
+
+func fuga(c *gin.Context) {
+	c.JSON(http.StatusOK, fugavar.id)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		fmt.Println("hoge")
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.GET("/fuga", fuga)
+
+	router.Run()
+}
